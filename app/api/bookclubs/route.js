e4b843ce69d7ecadf239426698e373bc8cc26334
Angela Palaszewski <delarosa.ang@gmail.com>
2023-06-24 12:30:06 -0600

    get and post requests for clubs

--- app/api/bookclubs/route.js
+++ app/api/bookclubs/route.js
@@ -1,1 +1,17 @@
 // rest api routes for get and post bookclubs
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+export async function GET(request) {
+    const bookclubs = await prisma.bookclub.findMany()
+    return NextResponse.json(bookclubs)
+}
+
+export async function POST(request) {
+    const json = await request.json()
+
+    const club = await prisma.bookclub.create({
+        data: json
+    })
+    return new NextResponse(JSON.stringify(club), { status: 201 })
+}
--- prisma/seed.js
+++ prisma/seed.js
@@ -29,6 +29,7 @@ const userData = [
   },
 ]
 
+
 async function main() {
   console.log(`Start seeding ...`)
   for (const u of userData) {
@@ -41,6 +42,46 @@ async function main() {
     })
     console.log(`Created user with id: ${user.id}`)
   }
+    const bookclub = await prisma.bookclub.createMany({
+        data: [
+            {
+                name: 'fakeclub',
+                location: 'virtual',
+                genre: 'romance',
+                description: 'fake description',
+                image: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+             
+            },
+            {
+                name: 'fakeclub2',
+                location: 'virtual',
+                genre: 'romance',
+                description: 'fake description',
+                image: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+           
+            }
+        ]
+    })
+    console.log('Created bookclubs', bookclub.count)
+  
+    const book = await prisma.book.createMany({
+        bookData: [
+            {
+                title: 'Fake Book',
+                author: 'Angela P',
+                coverImage: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+                subject: 'Romance'
+            },
+            {
+                title: 'Fake Book 2',
+                author: 'Nick P',
+                coverImage: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+                subject: 'Romance'
+            }
+        ]
+        })
+        console.log('Created books', book.count)
+    
   console.log(`Seeding finished.`)
 }
 
