010dc90df0f3ecc5797973b768593146eba8269b
Angela Palaszewski <delarosa.ang@gmail.com>
2023-06-25 13:20:34 -0600

    patch and delete books and clubs

--- app/api/bookclubs/[id]/route.js
+++ app/api/bookclubs/[id]/route.js
@@ -11,3 +11,26 @@ export async function GET(request, {params}) {
     })
     return NextResponse.json(bookclub)
 }
+
+export async function PATCH(request, {params}) {
+    const id = params.id
+    const json = await request.json()
+
+    const updateClub = await prisma.bookclub.update({
+        where: {
+            id: parseInt(id, 10),
+        }, 
+        data: json
+    })
+    return NextResponse.json(updateClub)
+}
+
+export async function DELETE(request, {params}) {
+    const id = params.id
+    const deleteClub = await prisma.bookclub.delete({
+        where: {
+            id: parseInt(id, 10),
+        }
+    })
+    return NextResponse.json(deleteClub)
+}
--- app/api/books/[id]/route.js
+++ app/api/books/[id]/route.js
@@ -12,3 +12,26 @@ export async function GET(request, {params}) {
     })
     return NextResponse.json(book)
 }
+
+export async function PATCH(request, {params}) {
+    const id = params.id
+    const json = await request.json()
+
+    const updateBook = await prisma.book.update({
+        where: {
+            id: parseInt(id, 10),
+        }, 
+        data: json
+    })
+    return NextResponse.json(updateBook)
+}
+
+export async function DELETE(request, {params}) {
+    const id = params.id
+    const deleteBook = await prisma.bookclub.delete({
+        where: {
+            id: parseInt(id, 10),
+        }
+    })
+    return NextResponse.json(deleteBook)
+}
