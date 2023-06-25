88ca381f744c436ab939f3048a258a7f2fd6bb71
Angela Palaszewski <delarosa.ang@gmail.com>
2023-06-25 12:44:12 -0600

    get book and club by id api routes

--- app/api/bookclubs/[id]/route.js
+++ app/api/bookclubs/[id]/route.js
@@ -1,1 +1,13 @@
 // get patch and delete 
+import prisma from '../../../../lib/prisma';
+import { NextResponse } from 'next/server'
+
+export async function GET(request, {params}) {
+    const id = params.id
+    const bookclub = await prisma.bookclub.findUnique({
+        where: {
+            id: parseInt(id, 10),
+        }
+    })
+    return NextResponse.json(bookclub)
+}
--- app/api/books/[id]/route.js
+++ app/api/books/[id]/route.js
@@ -1,3 +1,14 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
 
 
 
+export async function GET(request, {params}) {
+    const id = params.id
+    const book = await prisma.book.findUnique({
+        where: {
+            id: parseInt(id, 10),
+        }
+    })
+    return NextResponse.json(book)
+}
