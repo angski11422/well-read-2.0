e8f512f4d42873fb52106e7fbc4606acc82f7277
Angela Palaszewski <delarosa.ang@gmail.com>
2023-09-06 16:21:09 -0600

    dropdown animation

--- app/components/main-nav/main-nav.js
+++ app/components/main-nav/main-nav.js
@@ -1,35 +1,28 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-
-import Logo from './logo';
-import styles from './main-nav.module.scss';
-import UserNav from './user-nav';
-
-
+import Logo from "./logo";
+import styles from "./main-nav.module.scss";
+import UserNav from "./user-nav";
 
 export default function MainNav() {
-    
-    return (
-        <header className={styles.header}>
-            <div className={styles.mainnav}>
-                <Link href='/' className={styles.mainnav__logo}>
-                    <Logo />
-                </Link>
-                <Link href='/about' className={styles.mainnav__button}>
-                    <span>About</span>
-                </Link>
-                <Link href='/faq' className={styles.mainnav__button} >
-                    <span>FAQ</span>
-                </Link>
-                <Link href='/register' className={styles.mainnav__button} >
-                    <span>Create Account</span>
-                </Link>
-            </div>
-            <div className={styles.usernav}>
-                <div className={styles.usernav__iconbox}>
-                    <UserNav />
-                </div>  
-            </div>
-        </header> 
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles.mainnav}>
+        <Link href="/" className={styles.mainnav__logo}>
+          <Logo />
+        </Link>
+        <Link href="/about" className={styles.mainnav__button}>
+          <span>About</span>
+        </Link>
+        <Link href="/faq" className={styles.mainnav__button}>
+          <span>FAQ</span>
+        </Link>
+      </div>
+      <div className={styles.usernav}>
+        <div className={styles.usernav__iconbox}>
+          <UserNav />
+        </div>
+      </div>
+    </header>
+  );
 }
